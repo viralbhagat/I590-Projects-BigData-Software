4952cb91085cfe4345d3feed4980eed2af5677f6
Viral Bhagat <viral.bhagat@gmail.com>
2016-01-25 22:59:28 -0500

    changed the script to use function

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,13 +1,16 @@
 import sys
 
-number = 1
-while (number <= int(sys.argv[1])):
-	if number%2 == 0 and number%3 == 0:
-		print ('fizzbuzz')
-	elif number%2 == 0:
-		print ('fizz')
-	elif number%3 == 0:
-		print ('buzz')
-	else:
-		print (number)
-	number+=1
+def fizzbuzz(n):
+	number = 1
+	while (number <= n):
+		if number%2 == 0 and number%3 == 0:
+			print ('fizzbuzz')
+		elif number%2 == 0:
+			print ('fizz')
+		elif number%3 == 0:
+			print ('buzz')
+		else:
+			print (number)
+		number+=1
+
+fizzbuzz(int(sys.argv[1]))
